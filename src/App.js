bb7b33eecc30cd4185f8660b7eb3254c410378b1
Clint Maruti <clintmaruti@Clints-MacBook-Pro.local>
2022-10-11 12:19:07 +0300

    react-live-search

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [value, setValue] = useState("");
+    const [results, setResults] = useState([]);
+
+    const handleChange = (event) => {
+        const typedValue = event.target.value;
+        setValue(typedValue);
+    };
+
+    useEffect(() => {
+        if (value.length > 0) {
+            fetch("https://dummyjson.com/products")
+                .then((response) => response.json())
+                .then((responseData) => {
+                    setResults([]);
+                    let searchQuery = value.toLowerCase();
+                    if (responseData) {
+                        for (let item of responseData.products) {
+                            let product = item.title.toLowerCase();
+                            if (product.slice(0, searchQuery.length).indexOf(searchQuery) !== -1) {
+                                setResults((prevResults) => [...prevResults, product]);
+                            }
+                        }
+                    }
+                })
+                .catch((err) => console.log(err));
+        } else {
+            setResults([]);
+        }
+    }, [value]);
+
+    return (
+        <div className="App">
+            <h1>Please Enter Search Terms</h1>
+            <input className="form-input" value={value} onChange={handleChange} />
+            <div className="searchBack">
+                {results.length
+                    ? results.map((item) => (
+                          <div key={item} className="searchEntry">
+                              {item}
+                          </div>
+                      ))
+                    : null}
+            </div>
+        </div>
+    );
 }
 
 export default App;
